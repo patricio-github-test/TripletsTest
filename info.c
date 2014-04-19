7d97fb71af646c73fab5b1d0bf7dd36a42cc6fe8
Github Programmer 2 <github.programmer2@gmail.com>
2014-04-19 14:54:10 +0100

    3rd line to info.c

--- info.c
+++ info.c
@@ -3,7 +3,9 @@
 int main(void){
 	printf("info.c by Programmer 2 (linux)\n");
 	printf("Programmer 1 HERE!!\n");
-	pritnf("2nd line added by Programmer 2 (linux)\n");
+	printf("2nd line added by Programmer 2 (linux)\n");
+
+	printf("3rd line added by Programmer 2 (linux)\n");
 
 	return 0;
 }
