985008dd76d7c3b18a7c72a770888b7ceba5d394
angrybird <patdominguesPT@gmail.com>
2014-04-19 14:03:36 +0100

    prog1 modified info.c

--- info.c
+++ info.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(void){
-	printf("info.c by Programmer 2 (linux)\n");a
+	printf("info.c by Programmer 2 (linux)\n");
+	printf("Programmer 1 HERE!!\n");
 
 	return 0;
 }
